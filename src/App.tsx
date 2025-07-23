bd310e3a9918761e303c1aead18054e1374f8952
AlexKaplan <alexkaplan110606@gmail.com>
2025-07-23 16:11:26 +0300

    fix(styles): fix errors styles and add deploy settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import "./App.css";
+
 import { CrewList } from "./components/CrewList/CrewList";
 import { GoogleMapComponent } from "./components/GoogleMapComponent/GoogleMapComponent";
 import { ISSLocation } from "./components/ISSLocation/ISSLocation";
 import { TimeUTC } from "./components/TimeUTC/TimeUTC";
 import { useISSData } from "./hooks/useISSData";
-import "./App.css";
 function App() {
   const { position, crew, error } = useISSData();
 
   return (
     <div className="container-app">
-      <div className="location-box">
+      <div className="item-container left">
         <ISSLocation position={position} error={error} />
+      </div>
+
+      <div className="item-container right">
+        <TimeUTC />
+      </div>
+      <div className="item-container map left">
         {position && (
           <GoogleMapComponent
             latitude={position.latitude}
@@ -18,8 +25,8 @@ function App() {
           />
         )}
       </div>
-      <div className="time-and-crew-box">
-        <TimeUTC />
+
+      <div className="item-container right">
         <CrewList crew={crew} />
       </div>
     </div>
